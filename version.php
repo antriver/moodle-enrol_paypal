98b859a26c60b11acd7a6d8587deabf05dbb2439
Anthony Kuske <anthonykuske@gmail.com>
2015-06-18 02:13:54 +0200

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015061800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015061801;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015050500;        // Requires this Moodle version
 $plugin->component = 'enrol_paypalenhanced';    // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 60;
